86d3ce0ee709d989abae4e81ed872d5a400cf9f5
Sandeep Pabolu <pabolusandeep1@gmail.com>
2017-07-24 21:52:29 -0500

    lab7

---
+++ LAB7/source/src/main/scala/openie/SparkOpenIE.scala
@@ -0,0 +1,33 @@
+package openie
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+object SparkOpenIE {
+
+  def main(args: Array[String]) {
+    // Configuration
+    val conf = new SparkConf().setAppName("SparkWordCount").setMaster("local[*]")
+
+    val sparkContext = new SparkContext(conf)
+
+
+    // Turn off Info Logger for Console
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    val ipfile = sparkContext.textFile("data/sqad_data").map(s => {
+      //Getting OpenIE Form of the word using lda.CoreNLP
+
+      val output = MainNLPClass.returnTriplets(s)
+      output
+    })
+
+    //println(CoreNLP.returnTriplets("The dog has a lifespan of upto 10-12 years."))
+    println(ipfile.collect().mkString("\n"))
+
+
+  }
+
+}
